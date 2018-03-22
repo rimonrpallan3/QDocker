f69d95655eb4ab843bb18e869d29d6f9786f4fc1
rimon <rimonrpallan@gmail.com>
2018-03-22 22:57:47 +0530

    Working On User Uploading content Ui Page

--- app/src/main/java/com/voyager/qdocker/userUploadDoc/UserUploadDoc.java
+++ app/src/main/java/com/voyager/qdocker/userUploadDoc/UserUploadDoc.java
@@ -1,10 +1,16 @@
 package com.voyager.qdocker.userUploadDoc;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.voyager.qdocker.R;
 
 import butterknife.ButterKnife;
@@ -15,13 +21,24 @@ import butterknife.ButterKnife;
 
 public class UserUploadDoc extends AppCompatActivity {
 
-
+    Activity activity;
+    public Toolbar userUploadDocToolbar;
+    private FirebaseAuth mAuth;
+    String fireBaseToken;
+    Bundle bundle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_upload_doc);
         ButterKnife.bind(this);
+        mAuth = FirebaseAuth.getInstance();
+        fireBaseToken = FirebaseInstanceId.getInstance().getToken();
+        userUploadDocToolbar = (Toolbar) findViewById(R.id.userUploadDocToolbar);
+        setSupportActionBar(userUploadDocToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(getResources().getString(R.string.app_name));
+        userUploadDocToolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.black));
     }
 
     @Override
